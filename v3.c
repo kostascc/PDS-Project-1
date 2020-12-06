6c54348af88c18eda06c6d56a1f0ea45e99f639b
Kostas <kachatzis@ece.auth.gr>
2020-12-06 03:10:51 +0100

    Fix V3 Commentary

--- v3.c
+++ v3.c
@@ -25,10 +25,19 @@ void find_triangles_v3(int* mat, bool __show_c, bool __show_info)
    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
    
 
-   int found = 0;
-
+   // Get Size M of the Matrix
    int M = mat_get_M(mat);
 
+
+   // Triangles Found
+   int found = 0;
+
+   
+   /**
+    * Arrays d_i, d_j will be used as the
+    * lower arrays (columns of CSR).
+    * C is the final triangle count vector.
+    **/ 
    int *d_i = (int *)malloc(M*sizeof(int));
    if(d_i==NULL) exit(EXIT_FAILURE);
 
@@ -38,14 +47,28 @@ void find_triangles_v3(int* mat, bool __show_c, bool __show_info)
    int *c = (int *)calloc(M, sizeof(int));
    if(c==NULL) exit(EXIT_FAILURE);
 
-   int i_g_j = 0;
-
 
    
-   int j;
 
-   // i j k
-   for(j=0; j<M; j++)
+   /**
+    * The algorithm is as follows:
+    * 
+    * For each row j, find all the connected
+    * columns i. 
+    * Now every i column can be considered as
+    * a row i. For each such row, find the connected
+    * columns e.
+    * Any of the e columns that also exist in i,
+    * form a triangle.
+    * 
+    * Ha!
+    * 
+    * This requires any self loops to be removed
+    * in advance!
+    **/
+
+
+   for(int j=0; j<M; j++)
    {
 
       int size_d_j = M;
@@ -55,33 +78,37 @@ void find_triangles_v3(int* mat, bool __show_c, bool __show_info)
 
       if(size_d_j<1) continue;
 
-      // For each Column
+      // For each Column of j
       // Find edges to the other columns of j
       for(int i_indx=0; i_indx<size_d_j; i_indx++)
       {
 
+
+         // Column of j, becomes a i row
          int i = d_j[i_indx];
 
-         // if(i==j) continue;
 
+         // Find Columns of row i
          int size_d_i = M;
          mat_cols(mat, i, &d_i, &size_d_i);
 
+
+         // For each column found,
+         // Check if it already exists in i
          for(int e_indx=0; e_indx<size_d_i; e_indx++)
          {
             
+         
             int e = d_i[e_indx];
-
-            // if(e==i) continue;
                
             for(int k_indx=0; k_indx<size_d_j; k_indx++)
             {
                
                int k = d_j[k_indx];
 
-               // if(i==e||e==j) continue;
 
-               if(e==k){
+               if(e==k)       // e exists in i
+               {
                   found+=1;
                   c[i]++;
                   c[k]++;
@@ -97,12 +124,18 @@ void find_triangles_v3(int* mat, bool __show_c, bool __show_info)
    }
 
    
+
+   // Stop Timer
    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
    float delta_us = (float) ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000)/ (1000000);
 
+
+   // Show Info
    if(__show_info)
       printf(" > V3 took %f s, Found %d triangles.\n", delta_us, found); 
 
+
+   // Show c vector
    if(__show_c)
    {
       printf("\n");
@@ -113,12 +146,15 @@ void find_triangles_v3(int* mat, bool __show_c, bool __show_info)
       printf("\n\n");
    }
 
+
+
+   // Clean up
    free(c);
    free(d_i);
    free(d_j);
 
 
-   return;
+   return;  // I can't believe it worked..
 }
 
 
